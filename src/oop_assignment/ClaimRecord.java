e97a81762174ae87eb2e3e149ddfa61b6fdbf3d2
JasonLee99 <jasonlee-1999@hotmail.com>
2019-02-08 19:51:56 +0800

    Create ClaimRecord.java
    New class

---
+++ src/oop_assignment/ClaimRecord.java
@@ -0,0 +1,29 @@
+package oop_assignment;
+
+public class ClaimRecord {
+	public static void ClaimRecord() {
+		
+	}
+	
+	public static void ApplyClaim() {
+		
+	}
+	
+	public static void ApproveClaim() {
+		
+	}
+	
+	public static void CancelClaim() {
+		
+	}
+	
+	public static void DisplayClaim() {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
