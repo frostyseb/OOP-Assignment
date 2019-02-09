dd679426ad1860992dac85fc2846d40921e16ff7
JasonLee99 <jasonlee-1999@hotmail.com>
2019-02-09 18:46:28 +0800

    Update ClaimRecord.java
    Minor Updates
    WIP

--- src/oop_assignment/ClaimRecord.java
+++ src/oop_assignment/ClaimRecord.java
@@ -1,26 +1,24 @@
 package oop_assignment;
 import java.util.*;
-
+import java.text.*;
 public class ClaimRecord {
-	enum status {PENDING, APPROVED, REJECTED, CANCELLED}
-	public ClaimRecord() {
+	enum Status {
+		PENDING, APPROVED, REJECTED, CANCELLED;
+	}
+	Status stat;
+	public ClaimRecord(Status stat) {
 		//constructor
+		this.stat = stat;
 	}
 	
-	public static void ApplyClaim() {
+	public void ApplyClaim(float amount, float limit) {
+		//new claim object + details
 		//select claim type based on position
 		//enter amount & remark
 		//total amount of same type /> limit
-	}
-	
-	public static void EditClaim() {
-		//enter claimID to edit
-		//claimTypeID, amount, remark (editable)
-		//update Date
-	}
-	
-	public static void ApproveClaim(float amount) {
-		float limit = 0;
+		String approverID;
+		System.out.println("");
+
 		//A > B > C > D
 		if(amount < (0.5 * limit)) {
 			//C approver of D
@@ -36,24 +34,71 @@ public class ClaimRecord {
 		}
 	}
 	
-	public static void CancelClaim() {
-		//enter claimID to cancel claim
+	public void EditClaim() {
+		//enter claimID to edit
+		//claimTypeID, amount, remark (editable)
+		//update Date
+		String claimID, empID, claimTypeID, remark;
+		float amount;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("To edit Claim;");
+		System.out.print("Enter your Claim ID: ");
+		claimID = input.nextLine();
+		System.out.print("Enter your Employee ID: ");
+		empID = input.nextLine();
+		
+		System.out.println("You may edit your Claim type, amount & remark.");
+		
+		//editing part, unsure
+		
+		System.out.println("Enter your new Claim type: ");
+		claimTypeID = input.nextLine();
+		System.out.println("Enter your new amount: ");
+		amount = input.nextFloat();
+		System.out.println("Any remarks? ");
+		remark = input.nextLine();
+		
+		DisplayClaim();
+		
+	}
+	
+	public void ApproveClaim() {
 
 	}
 	
-	public static void DisplayClaim() {
+	public void CancelClaim() {
+		//enter claimID to cancel claim
+		DisplayClaim();
+		String claimID;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter your Claim ID to cancel the claim :");
+		claimID = input.nextLine();
+	}
+	
+	public void DisplayClaim() {
 		//list only PENDING
 		//show date
+	    SimpleDateFormat formatter = new SimpleDateFormat("YYYY-MM-DD");  
+	    Date date = new Date();
+		switch(stat) {
+			case PENDING:
+				//print out the claim records + date
+				break;
+			default:
+				break;
+		}
 
 	}
 	
-	public static void main(String[] args) {
-		String[] claimID = new String[20];
-		String[] empID = new String[20];
-		String[] claimTypeID = new String[20];
-		String[] approverID = new String[20];
-		String[] remark = new String[100];
-		String[] decisionRemark = new String[100];
+	public void main(String[] args) {
+		String claimID;
+		String empID;
+		String claimTypeID;
+		String approverID;
+		String remark;
+		String decisionRemark;
 		String date;
 		float amount;
 
